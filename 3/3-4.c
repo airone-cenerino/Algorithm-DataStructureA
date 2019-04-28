5d6a17dc3bb851701f1ab75f2ea80160b64dc962
Airone_Cenerino <tomopc723@yahoo.co.jp>
2019-04-28 09:45:58 +0900

    3回終了

--- 3/3-4.c
+++ 3/3-4.c
@@ -9,13 +9,13 @@ int main(int argc, char const *argv[])
         if(a==0 && b==0 && price ==0)
             break;
 
-        int moto = price / (1.0+a/100.0) + 2;   // Ø‚èÌ‚Ä+2‚©‚çl‚¦‚é
+        int moto = price / (1.0+a/100.0) + 2;   // ??????????????+2
 
         int ans=0;
         int i;
 
         while(ans==0){
-            for(i=1;i<moto/2+1;i++){    // moto‚ğ‚Q•ªŠ„‚·‚é
+            for(i=1;i<moto/2+1;i++){    // moto?2?????????
                 double bairitu1 = 1.0 + a / 100.0;
 
                 float num33 = (float)i*bairitu1;
@@ -24,7 +24,7 @@ int main(int argc, char const *argv[])
                 int num3 = num33;
                 int num4 = num44;
 
-                if(num3 + num4 == price){
+                if(num3 + num4 == price){   // ??????????????????
                     double bairitu2 = 1.0 + b / 100.0;
 
                     float num11 = (double)i * bairitu2;
@@ -32,7 +32,7 @@ int main(int argc, char const *argv[])
 
                     int num1 = num11;
                     int num2 = num22;
-                    if(ans < num1+num2){
+                    if(ans < num1+num2){    // ??????????
                         ans = num1 + num2;
                     }
                 }
